9bfbd880a20480e176d522a8c0072fdc11cfd082
joseph Evangelista <oootep123@gmail.com>
2021-03-16 00:11:53 -0700

    added comments

--- todo-app/src/app/app.module.ts
+++ todo-app/src/app/app.module.ts
@@ -1,3 +1,4 @@
+// this is where everything is being declared 
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule} from '@angular/forms'
@@ -6,12 +7,12 @@ import { AppComponent } from './app.component';
 import { TodosComponent } from './component/todos/todos.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
+  declarations: [ // declares the components
+    AppComponent,  // all our components will go in here
     TodosComponent
   ],
-  imports: [
-    BrowserModule,
+  imports: [ // imports go here
+    BrowserModule, // this would be our modules
     FormsModule
   ],
   providers: [],
